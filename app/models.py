bb35a4f0fbfce63aa3f95163421c89f857aef39a
Tyler <ttimms@kent.edu>
2018-10-17 22:28:46 -0400

    Completed chap 7

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
 
 @login.user_loader
 def load_user(id): # id is a string. 
@@ -13,7 +14,9 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
-
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
@@ -22,6 +25,11 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{d}?d=identicon&s={s}'.format(
+            d=digest, s=size)
 
 
 class Post(db.Model):
--- start.sh
+++
@@ -1,2 +0,0 @@
-export FLASK_APP=microblog.py
-flask run
